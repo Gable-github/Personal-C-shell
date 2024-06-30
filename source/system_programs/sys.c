131c69a82f9b56e7f8f91ce1bb17c714add40add
gable-github <gabryelsoh@gmail.com>
2024-06-30 12:41:00 +0800

    add sys.c to system programs

---
+++ source/system_programs/sys.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/utsname.h>
+#include <unistd.h>
+#include <pwd.h>
+#include <sys/sysinfo.h>
+
+void print_sys_info()
+{
+    struct utsname uname_data;
+    struct sysinfo sys_info;
+    struct passwd *pw;
+    char *user;
+
+    // get system information
+    if (uname(&uname_data) == -1)
+    {
+        perror("uname");
+        exit(EXIT_FAILURE);
+    }
+
+    // Get system uptime, load, etc.
+    if (sysinfo(&sys_info) == -1)
+    {
+        perror("sysinfo");
+        exit(EXIT_FAILURE);
+    }
+
+    // Get the current user
+    user = getenv("USER");
+    if (user == NULL)
+    {
+        pw = getpwuid(getuid());
+        if (pw)
+        {
+            user = pw->pw_name;
+        }
+    }
+
+    // Print system information
+    printf("Simple System Information\n");
+    printf("OS: %s\n", uname_data.sysname);
+    printf("Kernel: %s\n", uname_data.release);
+    printf("Hostname: %s\n", uname_data.nodename);
+    printf("Uptime: %ld days, %ld hours, %ld minutes\n", sys_info.uptime / 86400, (sys_info.uptime % 86400) / 3600, (sys_info.uptime % 3600) / 60);
+    printf("User: %s\n", user);
+    printf("CPU: %s\n", uname_data.machine);
+    printf("Total RAM: %lu MB\n", sys_info.totalram / 1024 / 1024);
+}
+
+int main()
+{
+    print_sys_info();
+    return 0;
+}
